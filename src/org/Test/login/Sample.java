baace331ca3b0d454ecac56b7464a4688658f403
rijju234 <muhammadrilwan@gmail.com>
2021-05-31 20:42:23 +0530

    String

---
+++ src/org/Test/login/Sample.java
@@ -0,0 +1,24 @@
+package org.Test.login;
+
+public class Sample {
+	
+
+	public static void main(String[] args) {
+		
+		String s ="Java application";
+		String s1 = "JaVa application";
+		
+		boolean equals = s.equals(s1);
+		System.out.println(equals);
+		
+		boolean equalsIgnoreCase = s.equalsIgnoreCase(s1);
+		System.out.println(equalsIgnoreCase);
+		
+		boolean startsWith = s.startsWith(s);
+		System.out.println(startsWith);
+		
+		int length = s.length();
+		System.out.println(length);
+
+}
+}
